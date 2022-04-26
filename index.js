a1c6de1f3dd637af59ab585bb5a8b154bbbeb4c1
Aaron Noe <aarminoe@gmail.com>
2022-04-26 10:25:09 -0500

    commit tree 6c67314d62ad90cb7533291b7dfc0e57a5782c22

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Aaron',
+    streetAddress: '123 Lane',
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmp = {...employee};
+    updatedEmp[key] = value;
+    return updatedEmp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmp = {...employee}
+    delete updatedEmp[key]
+    return updatedEmp
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
